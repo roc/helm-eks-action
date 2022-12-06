fd98ef9efa62551bdffb514f194a65e449587478
Ralph Cowling <ralph@ralph-cowling.com>
2022-12-06 13:55:29 +0100

    kubeconfig and github_output

--- dockerhub/entrypoint.sh
+++ dockerhub/entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-if [[ -n "${INPUT_PLUGINS// /}" ]]
-then
+echo ${KUBE_CONFIG_DATA} | base64 -d >kubeconfig
+export KUBECONFIG="${PWD}/kubeconfig"
+chmod 600 ${PWD}/kubeconfig
+
+if [[ -n "${INPUT_PLUGINS// /}" ]]; then
     plugins=$(echo $INPUT_PLUGINS | tr ",")
 
-    for plugin in $plugins
-    do
+    for plugin in $plugins; do
         echo "installing helm plugin: [$plugin]"
         helm plugin install $plugin
     done
@@ -17,4 +19,4 @@ echo "running entrypoint command(s)"
 
 response=$(sh -c " $INPUT_COMMAND")
 
-echo "::set-output name=response::$response"
+$GITHUB_OUTPUT="$GITHUB_OUTPUT response=$response"
